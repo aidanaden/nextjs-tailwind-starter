25cf3dadb129e9c2569101aff6c0ad720e4e8499
aidanaden <aidanaden@hotmail.com>
2022-04-07 04:19:31 +0800

    config: update tailwind with examples on extending fonts, colors and variants for lineClamp and scrollbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,41 @@ module.exports = {
   purge: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      // add custom fonts here
+      // moderat: 'Moderat, sans-serif',
+      // moderat_extended: 'Moderat Extended, sans-serif'
+    },
+    extend: {
+      colors: {
+        // extend colors here
+        // 'gv-yellow': '#FFD701',
+        // 'shaw-red': '#A50709',
+        // 'cathay-pink': '#CD0057',
+        // 'woodsmoke': {
+        //   '50': '#fafafa',
+        //   '100': '#f5f4f4',
+        //   '200': '#e8e3e3',
+        //   '300': '#dad2d2',
+        //   '400': '#ae9d9d',
+        //   '500': '#7e6d6d',
+        //   '600': '#5f4e4e',
+        //   '700': '#493c3c',
+        //   '800': '#2b2626',
+        //   '900': '#101014',
+        // },
+      },
+      lineClamp: {
+        7: '7',
+        8: '8',
+        9: '9',
+        10: '10',
+      }
+    },
   },
   variants: {
+    lineClamp: ['responsive'],
+    scrollbar: ['rounded'],
     extend: {},
   },
   plugins: [
